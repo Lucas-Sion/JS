8c7c54973ddeb4ece1b6dc339a53a88bcbbd39c0
Lucas sion <lucassantoslanzer@gmail.com>
2025-02-22 19:53:40 -0300

    Inicio

---
+++ Aula 2/aula2.js
@@ -0,0 +1,63 @@
+/* Variáveis
+
+    const
+    let
+    var
+
+*/
+
+/*const abacate = "Testando JS"
+
+const numero = 30
+
+console.log(abacate)
+console.log(numero)*/
+
+/* Tipos de Dados
+
+    String > Textos
+    Number > Números
+    Boolean > Booleano
+    Object > Objetos
+    Null & Undefined
+    Array > Matriz
+
+*/
+
+/*const numero = 30
+const texto = `o numero é ${numero}`
+
+console.log(texto)*/
+
+/*const numero = 250 / 5
+
+console.log(numero)*/
+
+/*const variavel = false*/
+
+/*const nome = "Lucas"
+const idade = "15"
+const casado = "false"*/
+
+/*const usuario = {
+    nome: 'Lucas',
+    idade: 15,
+    casado: false 
+
+}
+
+console.log(usuario.idade)*/
+
+/*const usuario = {
+    nome: 'Lucas',
+    idade: 15,
+    casado: true
+    conjuge: "Dona Bonita" 
+
+}
+
+console.log(usuario.abacate)*/
+
+const usuarios = [30, 40, 50, 60]
+
+console.log(usuarios[0])
