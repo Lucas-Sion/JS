023beb3a19396fdf324148a9a7aabb1942cb964e
Lucas sion <lucassantoslanzer@gmail.com>
2025-02-23 16:38:13 -0300

    IF & ELSE

---
+++ Aula 3/aula3.js
@@ -0,0 +1,43 @@
+/*
+
+If & Else > Controlador de Fluxo
+
+    If(SE)
+    Else(SE NÃO)
+
+    Operadores de Comparação
+        > maior que
+        < menor que
+        == igual que (Comparando SOMENTE o valor)
+        === igual que (Comparando o VALOR e o TIPO)
+        >= maior ou igual que
+        <= menor ou igual que
+ 
+  */
+
+  /*const notaDoAluno = 3
+
+
+  if(notaDoAluno === 10) {
+    console.log("Aluno NOTA 10")
+  } 
+
+  if(notaDoAluno < 5){
+    console.log("Tu é mt burro")
+
+  } */
+
+    const Sãopaulo = 300000
+    const Santos = 500
+
+    if(Sãopaulo >= 3000) {
+        console.log('É uma Cidade Grande')
+    } else{
+        console.log("É uma Cidade Pequena")
+    }
+
+    if(Santos >= 3000) {
+        console.log('É uma Cidade Grande')
+    } else{
+        console.log("É uma Cidade Pequena")
+    }
