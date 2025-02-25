fcb6603d47b1aad2b90d26c713dbf7b11b07d441
Lucas sion <lucassantoslanzer@gmail.com>
2025-02-24 22:11:48 -0300

    a

---
+++ Aula 4/aula4.js
@@ -0,0 +1,24 @@
+/*   Functions  / Funções
+        - Trecho de códigos que é executado
+         SOMENTE quando solicitado ( Parente gente boa)
+        - Trecho de código que pode ser Reutilizado várias vezes
+
+    [ ] O que é uma função e como usar
+    [ ] Função VOID (vazia)
+    [ ] Função com parâmetros
+    [ ] Função Return
+    [ ] Arrow Function
+*/
+const number = 200
+
+console.log(number)
+
+function digaMeuNome() {
+    console.log("Rodolfo Mori")
+}
+
+digaMeuNome()
+
+/* void */
+
+/* parâmentros */
