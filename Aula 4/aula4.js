5a2332a5fbb02262760df65197264162f6d2240a
Lucas sion <lucassantoslanzer@gmail.com>
2025-03-02 16:07:55 -0300

    Funçoes

--- Aula 4/aula4.js
+++ Aula 4/aula4.js
@@ -3,22 +3,51 @@
          SOMENTE quando solicitado ( Parente gente boa)
         - Trecho de código que pode ser Reutilizado várias vezes
 
-    [ ] O que é uma função e como usar
-    [ ] Função VOID (vazia)
-    [ ] Função com parâmetros
-    [ ] Função Return
+    [x] O que é uma função e como usar
+    [x] Função VOID (vazia) / Não RETORNA nada
+    [x] Função com parâmetros
+    [x] Função Return
     [ ] Arrow Function
 */
-const number = 200
 
-console.log(number)
-
-function digaMeuNome() {
-    console.log("Rodolfo Mori")
-}
-
-digaMeuNome()
 
 /* void */
 
+function digaMeuNome() {
+     console.log("Rodolfo Mori")
+}
+
 /* parâmentros */
+
+function soma(numero1, numero2) {
+    const resultado = numero1 + numero2
+
+    console.log(resultado)
+}
+
+
+/* return */
+
+function estaEndividado(receita, gastos) {
+    if (receita > gastos){
+        return 'Está no AZUL'
+    } else {
+        return 'Está no VERMELHO'
+    }
+}
+
+const Maria = estaEndividado(5000,7000)
+const Enzo = estaEndividado(10000,2000)
+
+console.log(Maria)
+console.log(Enzo)
+
+/* Arrow Function (Função flecha) */
+
+function abacate () {} // Forma clássica
+const digaMeuNome2 = (nome,nome2,nome3) => { // Forma Moderna
+    console.log('Rodolfo')
+
+}
+
+digaMeuNome2()
