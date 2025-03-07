3e4c0e57c2751033b32caf4baa9e05b49b6a8cf3
Lucas sion <lucassantoslanzer@gmail.com>
2025-03-07 20:58:58 -0300

    Math

---
+++ Aula 7/math.js
@@ -0,0 +1,12 @@
+/*
+        Math -> Matemática
+            - pow (2,2) / potência / 2² = 4
+            - sqrt (25,2) / raiz / √25
+            - ceil / teto -> arredondar pra cima
+            - floor / chão -> arredondar pra baixo
+            - random() / número aleatório entre 0 e 1
+
+*/
+
+const calculo = Math.random() * (100 - 1) + 1;
+console.log(Math.floor(calculo))
